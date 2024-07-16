5bae289ce8e1368a281637d67c6d0c683cb18cf4
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2024-07-16 17:50:51 -0300

    Update banco.js

--- src/banco.js
+++ src/banco.js
@@ -1,26 +1,26 @@
 import mysql2 from "mysql2";
 
 // Armazenando os dados da conexão em uma constante
-const conexao = mysql2.createConnection({
-    host : 'localhost',
-    user : 'root',
-    password : '',
-    database : 'pontofacil'
-});
+// const conexao = mysql2.createConnection({
+//     host : 'localhost',
+//     user : 'root',
+//     password : '',
+//     database : 'pontofacil'
+// });
 
 
 // Banco de dados DB4Free
-// const conexao = mysql2.createConnection({
-//     host : 'db4free.net',
-//     user : 'zimbosebastiao',
-//     password : 'Zimbo123@',
-//     database : 'apizimbo'
-// })
+const conexao = mysql2.createConnection({
+    host : 'db4free.net',
+    user : 'zimbosebastiao',
+    password : 'Zimbo123@',
+    database : 'apizimbo'
+})
 
 
 
 // Efetivando a conexão
-//conexao.connect();
+conexao.connect();
 conexao.connect(erro => {
     if(erro){
         console.error(`Erro ao conectar: ${erro.message}`);
